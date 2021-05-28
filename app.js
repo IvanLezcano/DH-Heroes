8877dfc495078223f1ea35ed4e893f208a2a128b
IvanLezcano <lezcanoivangabriel@gmail.com>
2021-05-28 15:51:56 -0300

    logrado llegar al navegador

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require ("express");
 const app = express();
 const port = 3001;
 
+app.get('/',(req,res)=>{
+    res.send('hola a todos ')
+})
 
 
 app.listen(port, () => console.log("servidor corriendo en el puierto3000"))
