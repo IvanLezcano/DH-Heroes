1e10d84a841b2e257ca40bbec722d40c1c80fff2
IvanLezcano <lezcanoivangabriel@gmail.com>
2021-06-01 18:23:54 -0300

    Primer commit

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
-const port = 3001;
+const port = 3030;
+const path = require("path");
+const views = path.join(__dirname,"views")
+console.log(__dirname);
 
-app.get('/',(req,res)=>{
-    res.send('hola a todos ')
-})
+
+app.get("/",(req,res) => res.sendFile(path.join(views,"index.html")))
+app.get("/home",(req,res) => res.sendFile(path.join(views,"index.html")))
+app.get("/babbage",(req,res) => res.sendFile(path.join(views,"babbage.html")))
+app.get("/berners-lee",(req,res) => res.sendFile(path.join(views,"berers-lee.html")))
+app.get("/clarke",(req,res) => res.sendFile(path.join(views,"clarke.html")))
+app.get("/hamilton",(req,res) => res.sendFile(path.join(views,"hamilton.html")))
+app.get("/hopper",(req,res) => res.sendFile(path.join(views,"hopper.html")))
+app.get("/lovelace",(req,res) => res.sendFile(path.join(views,"lovelace.html")))
+app.get("/turning",(req,res) => res.sendFile(path.join(views,"turning.html")))
 
 
-app.listen(port, () => console.log("servidor corriendo en el puierto3000"))
+
+
+
+app.listen(port,() => console.log("El servidor corriendo en el puerto"+ port))
+
+//npx nodemon app.js para ejecutarlo
