54b7eb27b98782e999ac6265ace5a92df006e1c6
IvanLezcano <lezcanoivangabriel@gmail.com>
2021-06-03 17:26:35 -0300

    commit random

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ console.log(__dirname);
 app.get("/",(req,res) => res.sendFile(path.join(views,"index.html")))
 app.get("/home",(req,res) => res.sendFile(path.join(views,"index.html")))
 app.get("/babbage",(req,res) => res.sendFile(path.join(views,"babbage.html")))
-app.get("/berners-lee",(req,res) => res.sendFile(path.join(views,"berers-lee.html")))
+app.get("/berners-lee",(req,res) => res.sendFile(path.join(views,"berners-lee.html")))
 app.get("/clarke",(req,res) => res.sendFile(path.join(views,"clarke.html")))
 app.get("/hamilton",(req,res) => res.sendFile(path.join(views,"hamilton.html")))
 app.get("/hopper",(req,res) => res.sendFile(path.join(views,"hopper.html")))
 app.get("/lovelace",(req,res) => res.sendFile(path.join(views,"lovelace.html")))
-app.get("/turning",(req,res) => res.sendFile(path.join(views,"turning.html")))
+app.get("/turing",(req,res) => res.sendFile(path.join(views,"turing.html")))
 
 
 
