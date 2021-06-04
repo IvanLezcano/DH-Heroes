9c4ae031828490e827d584bf3b11975ee1a11712
IvanLezcano <lezcanoivangabriel@gmail.com>
2021-06-04 15:02:54 -0300

    probando deploy

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 3030;
 const path = require("path");
 const views = path.join(__dirname,"views")
 app.use(express.static("public"));
 console.log(__dirname);
+
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3030;
+
+
+
 
 
 app.get("/",(req,res) => res.sendFile(path.join(views,"index.html")))
@@ -22,6 +30,10 @@ app.get("/trabajo-serio",(req,res) => res.sendFile(path.join(views,"trabajo-seri
 
 
 
-app.listen(port,() => console.log("El servidor corriendo en el puerto"+ port))
+app.listen(port, () => {
+    console.log(`Server is running on the port ${port}.`);
+  });
+  
+  
 
 //npx nodemon app.js para ejecutarlo
